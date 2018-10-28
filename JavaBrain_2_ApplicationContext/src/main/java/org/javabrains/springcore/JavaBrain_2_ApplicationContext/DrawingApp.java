a54c568fcd7ff34eebf928bf62b4d7ae678a6ba4
prasanna4 <prasannawadekar90@gmail.com>
2018-10-29 03:18:51 +0530

    Application context

---
+++ JavaBrain_2_ApplicationContext/src/main/java/org/javabrains/springcore/JavaBrain_2_ApplicationContext/DrawingApp.java
@@ -0,0 +1,24 @@
+package org.javabrains.springcore.JavaBrain_2_ApplicationContext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class DrawingApp 
+{
+    public static void main( String[] args )
+    {
+       //Triangle triangleobj=new Triangle(); 
+       //Triangle Drawn
+       //spring.xml
+       //BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
+       
+    	ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangleobj=(Triangle)context.getBean("triangle");
+        
+    	triangleobj.draw();
+    }
+}
---
+++ JavaBrain_2_ApplicationContext/src/main/java/org/javabrains/springcore/JavaBrain_2_ApplicationContext/Triangle.java
@@ -0,0 +1,7 @@
+package org.javabrains.springcore.JavaBrain_2_ApplicationContext;
+
+public class Triangle {
+   public void draw() {
+	   System.out.println("Triangle Drawn");
+   }
+}
