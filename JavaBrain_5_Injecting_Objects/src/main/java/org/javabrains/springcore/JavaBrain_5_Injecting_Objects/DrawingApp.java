32f87ca50ac0a6b91238c7ca374563261ad9c4c6
prasanna4 <prasannawadekar90@gmail.com>
2018-10-29 03:21:10 +0530

    Injecting Objects

---
+++ JavaBrain_5_Injecting_Objects/src/main/java/org/javabrains/springcore/JavaBrain_5_Injecting_Objects/DrawingApp.java
@@ -0,0 +1,20 @@
+package org.javabrains.springcore.JavaBrain_5_Injecting_Objects;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class DrawingApp 
+{
+    public static void main( String[] args )
+    {
+            
+    	ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangleobj=(Triangle)context.getBean("triangle");
+        
+    	triangleobj.draw();
+    }
+}
