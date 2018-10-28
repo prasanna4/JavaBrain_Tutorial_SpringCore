e0563f3ec1520f14126e31b781558991547a41a9
prasanna4 <prasannawadekar90@gmail.com>
2018-10-29 03:41:06 +0530

    autowired annotation part 2

---
+++ JavaBrain_20_Autowired_Annotation_Part2/src/main/java/org/javabrains/springcore/JavaBrain_20_Autowired_Annotation_Part2/DrawingApp.java
@@ -0,0 +1,21 @@
+package org.javabrains.springcore.JavaBrain_20_Autowired_Annotation_Part2;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class DrawingApp 
+{
+    public static void main( String[] args )
+    {
+            
+    	ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        //Shape shape=(Shape)context.getBean("circle");
+    	Shape shape=(Shape)context.getBean("circle");
+    	shape.draw();
+    }
+}
---
+++ JavaBrain_20_Autowired_Annotation_Part2/src/main/java/org/javabrains/springcore/JavaBrain_20_Autowired_Annotation_Part2/Shape.java
@@ -0,0 +1,7 @@
+package org.javabrains.springcore.JavaBrain_20_Autowired_Annotation_Part2;
+
+public interface Shape {
+  
+	public void draw();
+	
+}
